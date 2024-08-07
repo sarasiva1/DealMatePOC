11998fa3da0eb3c6535b13e5ad5e77fd56d0ccf1
Saravanan <Saravanans@bloomlync.com>
2024-08-07 12:48:18 +0530

    initail push for DealMate POC

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DealMate
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration configuration;
+        public ApplicationDbContext(IConfiguration configuration, DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            this.configuration = configuration;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
