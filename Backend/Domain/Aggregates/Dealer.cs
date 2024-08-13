9cb06d344609ae44a056a3c42bdb9d33e8da6843
Prathap M <prathapm@amtote.com>
2024-08-13 12:56:43 +0530

    feat:test pipeline

--- Backend/Domain/Aggregates/Dealer.cs
+++ Backend/Domain/Aggregates/Dealer.cs
@@ -4,5 +4,5 @@ public class Dealer
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
-    public string Address { get; set; } = null!;
+    public string Address { get; set; } = null!; 
 }
