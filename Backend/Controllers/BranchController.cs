13e0fd39f70432020fc1e06c9b0de7882904fc17
Arunachalam S <arunachalam.subramani@amtote.com>
2024-08-16 10:47:39 +0530

    Revert "feat: remove api"
    This reverts commit 8006e2a4d95ef2ecd3b31d612ff264e5bdf2673f.

--- Backend/Controllers/BranchController.cs
+++ Backend/Controllers/BranchController.cs
@@ -42,8 +42,16 @@ namespace DealMate.Backend.Controllers
             return Ok(await branchRepository.Update(branch));
         }
 
-        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await branchRepository.Delete(id));
+        }
 
-
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> TestDelete(int id)
+        {
+            return Ok(await branchRepository.Delete(id));
+        }
     }
 }
