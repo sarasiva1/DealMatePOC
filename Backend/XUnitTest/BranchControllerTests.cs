2ab3f97ec4c9949008b79afdf1585fa01e0e5115
Anandhappriya S <anandhappriya.saravanan@amtote.com>
2024-08-16 18:31:00 +0530

    Xunit test

---
+++ Backend/XUnitTest/BranchControllerTests.cs
@@ -0,0 +1,87 @@
+﻿using DealMate.Backend.Controllers;
+using DealMate.Backend.Domain.Aggregates;
+using DealMate.Backend.Infrastructure.Interfaces;
+using DealMate.Backend.Service.Common;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace DealMate.Backend.XUnitTest;
+
+public class BranchControllerTests
+{
+    private readonly Mock<IRepository<Branch>> _repository;
+    private readonly Mock<IBranchRepository> _branchRepository;
+    private readonly BranchController _controller;
+
+    public BranchControllerTests()
+    {
+        _repository = new Mock<IRepository<Branch>>();
+        _branchRepository = new Mock<IBranchRepository>();
+        _controller = new BranchController(_branchRepository.Object, _repository.Object);
+    }
+
+    private static Dealer TestDealer =>
+        new()
+        {
+            Id = 1,
+            Name = "TestDealer",
+            Address = "ABC"
+        };
+
+
+    private static Branch TestBranch =>
+        new()
+        {
+            Id = 1,
+            Name = "TestBranch",
+            DealerId = TestDealer.Id
+        };
+
+    [Fact]
+    public async Task Create_ReturnsOkResult()
+    {
+        // Arrange
+        var branch = TestBranch;
+        _branchRepository.Setup(repo => repo.Create(branch)).ReturnsAsync(branch);
+
+        // Act
+        var result = await _controller.Create(branch);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<Branch>(okResult.Value);
+        Assert.Equal(branch.DealerId, returnValue.DealerId);
+    }
+
+    [Fact]
+    public async Task Update_ReturnsOkResult()
+    {
+        // Arrange
+        var branch = TestBranch;
+        branch.Name = "TestBranch1";
+        _branchRepository.Setup(repo => repo.Update(branch)).ReturnsAsync(branch);
+
+        // Act 
+        var result = await _controller.Update(branch);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<Branch>(okResult.Value);
+        Assert.Equal(branch.Name, returnValue.Name);
+    }
+
+    [Fact]
+    public async Task Delete_ReturnsOkResult()
+    {
+        var branch = TestBranch;
+        // Arrange
+        _branchRepository.Setup(repo => repo.Delete(It.IsAny<int>())).ReturnsAsync(branch);
+
+        // Act
+        var result = await _controller.Delete(1);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+    }
+}
---
+++ Backend/XUnitTest/DealerControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using DealMate.Backend.Controllers;
+using DealMate.Backend.Domain.Aggregates;
+using DealMate.Backend.Infrastructure.Interfaces;
+using DealMate.Backend.Service.Common;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace DealMate.Backend.XUnitTest;
+
+public class DealerControllerTests
+{
+    private readonly Mock<IRepository<Dealer>> _repository;
+    private readonly Mock<IDealerRepository> _dealerRepository;
+    private readonly DealerController _controller;
+
+    public DealerControllerTests()
+    {
+        _repository = new Mock<IRepository<Dealer>>();
+        _dealerRepository = new Mock<IDealerRepository>();
+        _controller = new DealerController(_dealerRepository.Object, _repository.Object);
+    }
+
+    private static Dealer TestDealer =>
+        new()
+        {
+            Id = 1,
+            Name = "TestDealer",
+            Address = "ABC"
+        };
+
+    [Fact]
+    public async Task Create_ReturnsOkResult()
+    {
+        // Arrange
+        var dealer = TestDealer;
+        _dealerRepository.Setup(repo => repo.Create(dealer)).ReturnsAsync(dealer);
+
+        // Act
+        var result = await _controller.Create(dealer);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<Dealer>(okResult.Value);
+        Assert.Equal(dealer.Name, returnValue.Name);
+    }
+
+    [Fact]
+    public async Task Update_ReturnsOkResult()
+    {
+        // Arrange
+        var dealer = TestDealer;
+        dealer.Address = "IJK";
+        _dealerRepository.Setup(repo => repo.Update(dealer)).ReturnsAsync(dealer);
+
+        // Act 
+        var result = await _controller.Update(dealer);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<Dealer>(okResult.Value);
+        Assert.Equal("IJK", returnValue.Address);
+    }
+
+    [Fact]
+    public async Task Delete_ReturnsOkResult()
+    {
+        var dealer = TestDealer;
+        // Arrange
+        _dealerRepository.Setup(repo => repo.Delete(It.IsAny<int>())).ReturnsAsync(dealer);
+
+        // Act
+        var result = await _controller.Delete(1);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+    }
+}
